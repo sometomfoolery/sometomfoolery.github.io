38d2ee7d0a32e5209ca54763ec6aa14346b5b3ba
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2025-04-05 12:17:00 -0400

    Update script.js
    Added prepping comments for next feature

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     title.innerHTML = new_sentence;
 
+    // -- Give each letter a different color
+    // Genereate a list of colors
+
+    // Loop thru each letter, giving it a color from the list
+
     // Test: change 5th letter to red
     const span5 = title.querySelector('span#span5');
     span5.style += 'color: red;';
