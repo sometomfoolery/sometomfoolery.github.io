571776be1504711a51f9b9a5e7f2eb4d58a9e9d2
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2025-04-05 12:08:14 -0400

    Update script.js
    Testing changing one <p> element to red

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let new_sentence = "";
     for (let i = 0; i < sentence.length; i++)
     {
-        new_sentence += "<p>"
+        new_sentence += `<p id=\"p${i}\">`
         new_sentence += sentence[i];
         new_sentence += "</p>"
     }
+    title.innerHTML = new_sentence;
+
+    const p5 = title.getElementById('p5');
+    p5.style = 'color: red;';
 
 });
