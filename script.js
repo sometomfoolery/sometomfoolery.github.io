74c1a9d9d6fe517383f81d6ed0b4f59e02aaf607
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2025-04-05 12:19:35 -0400

    Update script.js
    Added test console logs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const title = document.getElementById('title');
 
     // Wrap each letter in its own <span> tag
+    console.log(title.innerHTML);
     const sentence = title.innerHTML;
     let new_sentence = "";
     for (let i = 0; i < sentence.length; i++)
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         new_sentence += "</span>";
     }
     title.innerHTML = new_sentence;
+    console.log(title.innerHTML);
 
     // -- Give each letter a different color
     // Genereate a list of colors
