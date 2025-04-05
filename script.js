0b535c3fdc09ec170aea45e08b5267362d2c4c3d
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2025-04-05 12:02:19 -0400

    Update script.js
    Wrap each letter in title in a <p> tag

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const title = document.getElementById('title');
-    title.innerHTML = "Butts! Lol!";
+
+    // Wrap each letter in its own <p> tag
+    const sentence = title.innerHTML;
+    let new_sentence = "";
+    for (let i = 0; i < sentence.length; i++)
+    {
+        new_sentence += "<p>"
+        new_sentence += sentence[i];
+        new_sentence += "</p>"
+    }
 
 });
